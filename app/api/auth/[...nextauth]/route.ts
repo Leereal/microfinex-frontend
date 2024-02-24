5ed14cfdd6f6a0bd98ff133015e892144841d028
unknown <leereal08@ymail.com>
2024-02-25 00:14:19 +0200

    fix : Next Auth pass httpOnly cookies to frontend

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,53 +1,13 @@
+import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import { NextRequest } from "next/server";
 
-const handler = NextAuth({
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/auth/login",
-    error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-  },
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials, req) {
-        const res = await fetch(
-          process.env.NEXT_PUBLIC_API_URL + "/auth/login/",
-          {
-            method: "POST",
-            body: JSON.stringify({
-              email: credentials?.email,
-              password: credentials?.password,
-            }),
-            headers: { "Content-Type": "application/json" },
-          }
-        );
+interface RouteHandlerContext {
+  params: { nextauth: string[] };
+}
 
-        const data = await res.json();
-        console.log("user", data);
-        if (res.ok && data) {
-          return data;
-        }
-        return null;
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      return { ...token, ...user };
-    },
-    async session({ session, token, user }) {
-      session.data = token as any;
-      return session;
-    },
-  },
-});
+const handler = async (req: NextRequest, context: RouteHandlerContext) => {
+  return NextAuth(req, context, authOptions);
+};
 
 export { handler as GET, handler as POST };
